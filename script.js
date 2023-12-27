2eb249879ebd64812226041477a7c5ff23b2ee3e
Joshua Masangcay <josh.tanay@gmail.com>
2023-12-27 16:03:59 +0800

    Updated the newCard and startGame function

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -22,6 +20,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -46,8 +49,12 @@ function renderGame() {
 
 
 function newCard() {
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    renderGame()
+    if (hasBlackJack === false && isAlive === true) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    } else {
+
+    }
 }
